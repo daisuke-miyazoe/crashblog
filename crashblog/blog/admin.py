1869b8e288eec477643cc9824523d56d8685c948
だい <dai@dainonotobukkukonpyuta.local>
2024-09-23 21:49:05 +0900

    6

--- crashblog/blog/admin.py
+++ crashblog/blog/admin.py
@@ -2,6 +2,28 @@ from django.contrib import admin
 
 from .models import Post, Category, Comment
 
-admin.site.register(Post)
-admin.site.register(Category)
-admin.site.register(Comment)
+class CommentItemInline(admin.TabularInline):
+    model = Comment
+    raw_id_fields = ['post']
+
+class PostAdmin(admin.ModelAdmin):
+    search_fields = ['title', 'intro', 'body']
+    list_display = ['title', 'slug', 'category', 'created_at']
+    list_filter = ['category', 'created_at']
+    inlines = [CommentItemInline]
+    prepopulated_fields = {'slug': ('title',)}
+
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ['title']
+    list_display = ['title']
+    # list_filter = ['category', 'created_at']
+    prepopulated_fields = {'slug': ('title',)}
+
+class CommentAdmin(admin.ModelAdmin):
+    # search_fields = ['title']
+    list_display = ['name', 'post', 'created_at']
+    # list_filter = ['category', 'created_at']
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Comment, CommentAdmin)

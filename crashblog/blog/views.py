24e678519edb40f1e8c80732b2056e59000e3296
だい <dai@dainonotobukkukonpyuta.local>
2024-10-01 23:35:01 +0900

    7

--- crashblog/blog/views.py
+++ crashblog/blog/views.py
@@ -1,10 +1,11 @@
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404, redirect
 
 from .forms import CommentForm
 from .models import Post, Category
 
 def detail(request, category_slug, slug):
-    post = get_object_or_404(Post, slug=slug)
+    post = get_object_or_404(Post, slug=slug, status=Post.ACTIVE)
     if request.method == 'POST':
         form = CommentForm(request.POST)
         if form.is_valid():
@@ -19,4 +20,10 @@ def detail(request, category_slug, slug):
 
 def category(request, slug):
     category = get_object_or_404(Category, slug=slug)
-    return render(request, 'blog/category.html', {'category': category,})
+    posts = category.posts.filter(status=Post.ACTIVE)
+    return render(request, 'blog/category.html', {'category': category, 'posts': posts})
+
+def search(request):
+    query = request.GET.get('query', '')
+    posts = Post.objects.filter(status=Post.ACTIVE).filter(Q(title__icontains=query) | Q(intro__icontains=query))
+    return render(request, 'blog/search.html', {'posts': posts, 'query': query})
